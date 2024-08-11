fa0993778ff43a89356f2aa109dc6d8c14745506
xingcdev <60323726+xingcdev@users.noreply.github.com>
2024-08-11 16:46:27 +0800

    refactor: change timestamp format

--- src/main/java/com/xingcdev/museum/domain/dto/ErrorDto.java
+++ src/main/java/com/xingcdev/museum/domain/dto/ErrorDto.java
@@ -3,13 +3,13 @@ package com.xingcdev.museum.domain.dto;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 
 @Getter
 @Builder
 public class ErrorDto {
     @Builder.Default
-    private final String timestamp = new Date().toString();
+    private final String timestamp = LocalDateTime.now().toString();
     private final String code;
     private final String message;
 }
--- src/main/java/com/xingcdev/museum/domain/dto/ErrorDtoWithDetails.java
+++ src/main/java/com/xingcdev/museum/domain/dto/ErrorDtoWithDetails.java
@@ -3,14 +3,14 @@ package com.xingcdev.museum.domain.dto;
 import lombok.Builder;
 import lombok.Getter;
 
-import java.util.Date;
+import java.time.LocalDateTime;
 import java.util.List;
 
 @Getter
 @Builder
 public class ErrorDtoWithDetails {
     @Builder.Default
-    private final String timestamp = new Date().toString();
+    private final String timestamp = LocalDateTime.now().toString();
     private final String code;
     private final String message;
     private final List<DetailDto> details;
