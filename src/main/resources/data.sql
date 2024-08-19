41ef5330f766b084b81eacb879d89c1675a918cb
xingcdev <60323726+xingcdev@users.noreply.github.com>
2024-08-19 15:54:46 +0800

    refactor: remove visits data in data.sql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -10,11 +10,3 @@ VALUES ('dc739b92-f0a0-47bf-a49e-0f6296dc726c', 'musée du bois', 'place de la R
 INSERT INTO museum
 VALUES ('dc739b92-f0a0-47bf-a49e-0f6296dc726d', 'musée Picasso', '5, rue Thorigny', '75003', 'Paris', 'Paris',
         '01 42 71 25 21', 'https://www.museepicassoparis.fr/', '48.8597', '2.362644');
-INSERT INTO visit (id, comment, museum_id, user_id)
-VALUES ('b32d7e60-2d3b-4fd0-af93-9ffdc3dd41a4', 'Comment 1', 'dc739b92-f0a0-47bf-a49e-0f6296dc726a',
-        'ba5bb00d-595f-4661-aa7d-21cfbdd5d103');
-INSERT INTO visit (id, comment, museum_id, user_id)
-VALUES ('44636ec1-3012-48be-86de-7ee8ded93b50', 'Comment 2', 'dc739b92-f0a0-47bf-a49e-0f6296dc726b', 'xxx');
-INSERT INTO visit (id, comment, museum_id, user_id)
-VALUES ('ef3be41b-4f50-4f38-9cd3-74187ce8a2db', 'Comment 3', 'dc739b92-f0a0-47bf-a49e-0f6296dc726c',
-        'ba5bb00d-595f-4661-aa7d-21cfbdd5d103');
