a3d4108963c8116ff6016ad4b009863ccef0d1ba
xingcdev <60323726+xingcdev@users.noreply.github.com>
2024-09-17 18:30:41 +0200

    refactor: remove public Swagger url in production

--- src/main/java/com/xingcdev/museum/config/SecurityConfig.java
+++ src/main/java/com/xingcdev/museum/config/SecurityConfig.java
@@ -3,6 +3,7 @@ package com.xingcdev.museum.config;
 import com.xingcdev.museum.exceptions.handler.CustomAuthenticationEntryPoint;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
@@ -20,7 +21,21 @@ public class SecurityConfig {
     }
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+    @Profile("production")
+    public SecurityFilterChain filterChainProduction(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults())
+                .authorizeHttpRequests((authorize) -> authorize
+                        .anyRequest()
+                        .authenticated()
+                )
+                .oauth2ResourceServer((oauth2) -> oauth2.jwt(Customizer.withDefaults()))
+                .exceptionHandling((exception) -> exception.authenticationEntryPoint(customAuthenticationEntryPoint));
+        return http.build();
+    }
+
+    @Bean
+    @Profile("development")
+    public SecurityFilterChain filterChainDevelopment(HttpSecurity http) throws Exception {
         http.cors(Customizer.withDefaults())
                 .authorizeHttpRequests((authorize) -> authorize
                         .requestMatchers("/swagger-ui/**", "/v3/api-docs/**").permitAll()
